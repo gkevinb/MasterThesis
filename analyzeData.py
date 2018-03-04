7eb126e2569ab5b9043b9eea601123913bdc1c0f
Gabor <gkbarta@yahoo.com>
2018-03-04 17:13:12 +0100

    Created a class for the time series to import it from file.
    Organized the code for importing the time series from a txt file.

--- analyzeData.py
+++ analyzeData.py
@@ -1,24 +1,45 @@
-file = open('testfile.txt', 'r')
+import collections
 
 
-topEvent = []
-basicEvents = []
+class TimeSeries:
 
-line = file.readline()
-for time in line.split():
-    topEvent.append(float(time))
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.time_series = {}
+        self.top_event_index = 0
+        self.basic_event_start_index = 1
+        self.number_of_basic_events = self.read_time_series_from_file()
 
-lines = file.readlines()
-for line in lines:
-    basicEvent = []
-    for time in line.split():
-        basicEvent.append(float(time))
-    basicEvents.append(basicEvent)
+    '''
+    Reads time series from the file given in the __init__ function.
+    Places the times series of the top event as the first element in 
+    the dictionary and then all the basic events follow. The function
+    returns the number of basic events found in the file.
+    '''
+    def read_time_series_from_file(self):
+        file = open(self.file_name, 'r')
+        dict_time_series = {}
+        index = 0
 
-# Print time series
-print(topEvent)
-for basicEvent in basicEvents:
-    print(basicEvent)
+        lines = file.readlines()
+        for line in lines:
+            event_time_series = []
+            for time in line.split():
+                event_time_series.append(float(time))
+            dict_time_series[index] = event_time_series
+            index += 1
+
+            self.time_series = collections.OrderedDict(sorted(dict_time_series.items()))
+
+        file.close()
+        return index - 1
+
+    def display_event_time_series(self):
+        print('Top Event : ' + str(self.time_series[self.top_event_index]))
+        for i in range(self.basic_event_start_index, len(self.time_series)):
+            print('Basic Event ' + str(i) + ' : ' + str(self.time_series[i]))
 
 
-file.close()
+time_series = TimeSeries('testfile.txt')
+time_series.display_event_time_series()
+# print('Number of basic event: ' + str(time_series.number_of_basic_events))
