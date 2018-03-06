8579d4377e96a5053970807a02935873b7aaa436
Gabor <gkbarta@yahoo.com>
2018-03-06 18:19:47 +0100

    Worked on analyzing time series.
    Able to get mean time to failure and mean time to repar from the time series stream for each event.

--- analyzeData.py
+++ analyzeData.py
@@ -1,6 +1,10 @@
 import collections
 
 
+is_EVEN = lambda i: i % 2 == 0
+is_ODD = lambda i: i % 2 == 1
+
+
 class TimeSeries:
 
     def __init__(self, file_name):
@@ -39,7 +43,41 @@ class TimeSeries:
         for i in range(self.basic_event_start_index, len(self.time_series)):
             print('Basic Event ' + str(i) + ' : ' + str(self.time_series[i]))
 
+    def get_time_differences(self, event):
+        event_time_series = self.time_series[event]
+        time_difference = 0
+        times = []
+        for time in event_time_series:
+            times.append(time - time_difference)
+            time_difference = time
+        #print(times)
+        return times
 
-time_series = TimeSeries('testfile.txt')
+    def get_mean_time_to_failure(self, event):
+        times = self.get_time_differences(event)
+        time_to_failures = []
+        for i in range(len(times)):
+            if is_EVEN(i):
+                time_to_failures.append(times[i])
+        print(time_to_failures)
+        mean_time_to_failure = sum(time_to_failures)/len(time_to_failures)
+        return mean_time_to_failure
+
+    def get_mean_time_to_repair(self, event):
+        times = self.get_time_differences(event)
+        time_to_repair = []
+        for i in range(len(times)):
+            if is_ODD(i):
+                time_to_repair.append(times[i])
+        print(time_to_repair)
+        mean_time_to_failure = sum(time_to_repair)/len(time_to_repair)
+        return mean_time_to_failure
+
+
+time_series = TimeSeries('time_series.txt')
+# time_series = TimeSeries('testfile.txt')
 time_series.display_event_time_series()
 # print('Number of basic event: ' + str(time_series.number_of_basic_events))
+EVENT = 0
+print(time_series.get_mean_time_to_failure(EVENT))
+print(time_series.get_mean_time_to_repair(EVENT))
