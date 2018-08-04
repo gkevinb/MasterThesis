8992c36c3b0ffb965ac5b78ce2d8fc1e7212fcf6
Unknown <gkbarta@yahoo.com>
2018-08-04 11:30:31 +0200

    Minor changes.

--- fault_trees.py
+++ fault_trees.py
@@ -41,19 +41,21 @@ def B():
 
 
 def C():
+    rel_exp = ['EXP', 1 / 9]
+
     topEvent = Event('Top Event')
     and1 = Gate('AND', parent=topEvent)
     intermediateEvent1 = Event('Intermediate Event 1', parent=and1)
     intermediateEvent2 = Event('Intermediate Event 2', parent=and1)
     vote2 = Gate('VOTING', parent=intermediateEvent1, k=2)
-    basicEvent1 = Event('Basic Event 1', rel_exp_dist, main_exp_dist, parent=vote2)
-    basicEvent2 = Event('Basic Event 2', rel_exp_dist, main_exp_dist, parent=vote2)
+    basicEvent1 = Event('Basic Event 1', rel_exp, main_exp_dist, parent=vote2)
+    basicEvent2 = Event('Basic Event 2', rel_exp, main_exp_dist, parent=vote2)
     intermediateEvent3 = Event('Intermediate Event 3', parent=vote2)
     and2 = Gate('AND', parent=intermediateEvent3)
     basicEvent3 = Event('Basic Event 3', rel_exp_dist, main_exp_dist, parent=and2)
     basicEvent4 = Event('Basic Event 4', rel_exp_dist, main_exp_dist, parent=and2)
     or1 = Gate('OR', parent=intermediateEvent2)
-    basicEvent5 = Event('Basic Event 5', rel_exp_dist, main_exp_dist, parent=or1)
+    basicEvent5 = Event('Basic Event 5', rel_exp, main_exp_dist, parent=or1)
     intermediateEvent4 = Event('Intermediate Event 4', parent=or1)
     and3 = Gate('AND', parent=intermediateEvent4)
     basicEvent6 = Event('Basic Event 6', rel_exp_dist, main_exp_dist, parent=and3)
@@ -72,7 +74,7 @@ def D():
     intermediateEvent2 = Event('Intermediate Event 2', parent=or1)
     vote2 = Gate('VOTING', parent=intermediateEvent1, k=2)
     basicEvent1 = Event('Basic Event 1', lognorm_dist, norm_dist, parent=vote2)
-    basicEvent2 = Event('Basic Event 2', rel_exp_dist, weibull_dist, parent=vote2)
+    basicEvent2 = Event('Basic Event 2', ['EXP', 1 / 10], weibull_dist, parent=vote2)
     basicEvent3 = Event('Basic Event 3', rel_exp_dist, main_exp_dist, parent=vote2)
     and2 = Gate('AND', parent=intermediateEvent2)
     basicEvent4 = Event('Basic Event 4', norm_dist, main_exp_dist, parent=and2)
@@ -84,7 +86,6 @@ def D():
 
 
 def E():
-
     topEvent = Event('Top Event')
     and1 = Gate('AND', parent=topEvent)
     intermediateEvent1 = Event('Intermediate Event 1', parent=and1)
@@ -118,7 +119,7 @@ def F():
     basic_event_5 = Event("Basic Event 5", lognorm_dist, main_exp_dist, parent=voting2)
     or3 = Gate("OR", parent=intermediate_event_2)
     basic_event_1 = Event("Basic Event 1", norm_dist, main_exp_dist, parent=or3)
-    basic_event_2 = Event("Basic Event 2", rel_exp_dist, norm_dist, parent=or3)
+    basic_event_2 = Event("Basic Event 2", ['EXP', 1 / 3], norm_dist, parent=or3)
 
     FT = FaultTree(top_event)
 
@@ -156,7 +157,7 @@ def H():
     intermediate_event_2 = Event("Intermediate Event 2", parent=and1)
     and2 = Gate("AND", parent=intermediate_event_1, k=2)
     basic_event_1 = Event("Basic Event 1", norm_dist, main_exp_dist, parent=and2)
-    basic_event_2 = Event("Basic Event 2", rel_exp_dist, norm_dist, parent=and2)
+    basic_event_2 = Event("Basic Event 2", ['EXP', 1 / 10], norm_dist, parent=and2)
     or3 = Gate("OR", parent=intermediate_event_2)
     basic_event_3 = Event("Basic Event 3", norm_dist, main_exp_dist, parent=or3)
     basic_event_4 = Event("Basic Event 4", lognorm_dist, weibull, parent=or3)
--- generatedFT_method.py
+++ generatedFT_method.py
@@ -8,10 +8,12 @@ def build_fault_tree():
     intermediate_event_1 = Event("Intermediate Event 1", parent=and1)
     intermediate_event_2 = Event("Intermediate Event 2", parent=and1)
     and2 = Gate("AND", parent=intermediate_event_1)
-    basic_event_1 = Event("Basic Event 1", parent=and2)
-    basic_event_2 = Event("Basic Event 2", parent=and2)
+    basic_event_4 = Event("Basic Event 4", parent=and2)
+    basic_event_5 = Event("Basic Event 5", parent=and2)
+    basic_event_6 = Event("Basic Event 6", parent=and2)
     or3 = Gate("OR", parent=intermediate_event_2)
+    basic_event_1 = Event("Basic Event 1", parent=or3)
+    basic_event_2 = Event("Basic Event 2", parent=or3)
     basic_event_3 = Event("Basic Event 3", parent=or3)
-    basic_event_4 = Event("Basic Event 4", parent=or3)
 
     return top_event
