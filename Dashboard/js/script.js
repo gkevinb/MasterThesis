8eb065fcbdeee49f68cf4cffbf163e95aee60c29
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-09-30 14:25:14 +0200

    Add gulp and make vue app work

--- Dashboard/js/script.js
+++ Dashboard/js/script.js
@@ -1,13 +1,8 @@
-var last_chosen_event = 'Basic Event 1';
-var last_chosen_metric = 'Reliability';
-var original_fault_tree;
-var reconstructed_fault_tree;
-var timeout;
-
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
-        renderKey: 1,
+        lastChosenEvent: 'Basic Event 1',
+        lastChosenMetric: 'Reliability',
         OriginalFaultTree: null,
         ReconstructedFaultTree: null,
         MinimalCutSets: null
@@ -23,115 +18,88 @@ var app = new Vue({
                 this.MinimalCutSets = response.data.MinimalCutSets;
             });
     },
-    watch: {
-        OriginalFaultTree: function () {
-            this.renderKey += 1
+    methods: {
+        round: function(value, decimals) {
+            return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
         },
-        MinimalCutSets: function () {
-            this.renderKey += 1
+        getBasicEventNumber: function (basicEventName) {
+            return basicEventName.slice(12, basicEventName.length);
         },
-    },
-})
-
-
-function round(value, decimals) {
-    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
-}
-
-function formatDistribution(distribution) {
-    if (distribution[0] == 'EXP') {
-        var lambda = round(distribution[1], 5);
-        return 'Exp(&lambda; = '.concat(lambda).concat(')');
-    }
-    if (distribution[0] == 'WEIBULL') {
-        scale = round(distribution[1], 5);
-        shape = round(distribution[2], 5);
-        return 'Weibull(&alpha; = '.concat(scale).concat(', &beta; = ').concat(shape).concat(')');
-    }
-    if (distribution[0] == 'NORMAL') {
-        mu = round(distribution[1], 5);
-        sigma = round(distribution[2], 5);
-        return 'Normal(&mu; = '.concat(mu).concat(', &sigma; = ').concat(sigma).concat(')');
-    }
-    if (distribution[0] == 'LOGNORM') {
-        mu = round(distribution[1], 5);
-        sigma = round(distribution[2], 5);
-        return 'Lognormal(&mu; = '.concat(mu).concat(', &sigma; = ').concat(sigma).concat(')');
-    }
-    if (distribution[0] == 'UNIDENTIFIED DISTRIBUTION') {
-        return 'N/A';
-    }
-}
-
-function getBasicEventNumber(basicEventName) {
-    var id = basicEventName.slice(12, basicEventName.length);
-    return id
-}
-
-function EventRadioButtonListener() {
-    $(document).ready(function () {
-        $("input:radio[name=events]").click(function () {
-            var event = $(this).val();
-            last_chosen_event = event
-            fillEventInformation(event)
-            document.getElementById("event_title").innerHTML = event;
-            var metric = last_chosen_metric;
-            var image_name = event.toLowerCase().replace(/ /g, "_");
-            var directory = '/MasterThesis/static/images/';
-            var png = '.png';
-            var source = directory.concat(image_name.concat('_'.concat(metric.concat(png))));
-            $('#plot').attr('src', source);
-        });
-    });
-}
-
-function MetricRadioButtonListener() {
-    $(document).ready(function () {
-        $("input:radio[name=metric]").click(function () {
-            console.log("metric radio button listener")
-            var metric = $(this).val();
-            last_chosen_metric = metric;
-            var event = last_chosen_event;
-            var image_name = event.toLowerCase().replace(/ /g, "_");
-            var directory = '/MasterThesis/static/images/';
-            var png = '.png';
-            var source = directory.concat(image_name.concat('_'.concat(metric.concat(png))));
+        metricRadioButton: function (metric) {
+            console.log("NEW metric radio button listener")
+            this.lastChosenMetric = metric;
+            let event = this.lastChosenEvent;
+            let image_name = event.toLowerCase().replace(/ /g, "_");
+            let directory = '/MasterThesis/static/images/';
+            let png = '.png';
+            let source = directory.concat(image_name.concat('_'.concat(metric.concat(png))));
             console.log(source)
             $('#plot').attr('src', source);
-        });
-    });
-}
-
-function init() {
-    EventRadioButtonListener();
-    MetricRadioButtonListener();
-}
-
-function fillEventInformation(event) {
-    if (event == 'Top Event') {
-        document.getElementById("oft_rel_dis").innerHTML = "";
-        document.getElementById("rft_rel_dis").innerHTML = "";
-        document.getElementById("oft_main_dis").innerHTML = "";
-        document.getElementById("rft_main_dis").innerHTML = "";
-        document.getElementById("oft_mtbf").innerHTML = "";
-        document.getElementById("rft_mtbf").innerHTML = round(reconstructed_fault_tree[0].mtbf, 5);
-        document.getElementById("oft_mtbr").innerHTML = "";
-        document.getElementById("rft_mtbr").innerHTML = round(reconstructed_fault_tree[0].mtbr, 5);
-        oper_avail_string = round(reconstructed_fault_tree[0].oper_avail * 100, 5).toString();
-        document.getElementById("oper_avail").innerHTML = oper_avail_string.concat(' %');
-    } else {
-        basicEventID = getBasicEventNumber(event);
-        document.getElementById("oft_rel_dis").innerHTML = formatDistribution(original_fault_tree[basicEventID].reliability);
-        document.getElementById("rft_rel_dis").innerHTML = formatDistribution(reconstructed_fault_tree[basicEventID].reliability);
-        document.getElementById("oft_main_dis").innerHTML = formatDistribution(original_fault_tree[basicEventID].maintainability);
-        document.getElementById("rft_main_dis").innerHTML = formatDistribution(reconstructed_fault_tree[basicEventID].maintainability);
-        document.getElementById("oft_mtbf").innerHTML = round(original_fault_tree[basicEventID].mtbf, 5);
-        document.getElementById("rft_mtbf").innerHTML = round(reconstructed_fault_tree[basicEventID].mtbf, 5);
-        document.getElementById("oft_mtbr").innerHTML = round(original_fault_tree[basicEventID].mtbr, 5);
-        document.getElementById("rft_mtbr").innerHTML = round(reconstructed_fault_tree[basicEventID].mtbr, 5);
-        oper_avail_string = round(reconstructed_fault_tree[basicEventID].oper_avail * 100, 5).toString();
-        document.getElementById("oper_avail").innerHTML = oper_avail_string.concat(' %');
+        },
+        eventRadioButton: function (event) {
+            console.log('new evetn list')
+            console.log(event)
+            this.lastChosenEvent = event
+            this.fillEventInformation(event)
+            document.getElementById("event_title").innerHTML = event;
+            let metric = this.lastChosenMetric;
+            let image_name = event.toLowerCase().replace(/ /g, "_");
+            let directory = '/MasterThesis/static/images/';
+            let png = '.png';
+            let source = directory.concat(image_name.concat('_'.concat(metric.concat(png))));
+            $('#plot').attr('src', source);
+        },
+        formatDistribution: function(distribution) {
+            if (distribution[0] == 'EXP') {
+                let lambda = this.round(distribution[1], 5);
+                return 'Exp(&lambda; = '.concat(lambda).concat(')');
+            }
+            if (distribution[0] == 'WEIBULL') {
+                scale = this.round(distribution[1], 5);
+                shape = this.round(distribution[2], 5);
+                return 'Weibull(&alpha; = '.concat(scale).concat(', &beta; = ').concat(shape).concat(')');
+            }
+            if (distribution[0] == 'NORMAL') {
+                mu = this.round(distribution[1], 5);
+                sigma = this.round(distribution[2], 5);
+                return 'Normal(&mu; = '.concat(mu).concat(', &sigma; = ').concat(sigma).concat(')');
+            }
+            if (distribution[0] == 'LOGNORM') {
+                mu = this.round(distribution[1], 5);
+                sigma = this.round(distribution[2], 5);
+                return 'Lognormal(&mu; = '.concat(mu).concat(', &sigma; = ').concat(sigma).concat(')');
+            }
+            if (distribution[0] == 'UNIDENTIFIED DISTRIBUTION') {
+                return 'N/A';
+            }
+        },
+        fillEventInformation: function (event) {
+            if (event == 'top_event') {
+                document.getElementById("oft_rel_dis").innerHTML = "";
+                document.getElementById("rft_rel_dis").innerHTML = "";
+                document.getElementById("oft_main_dis").innerHTML = "";
+                document.getElementById("rft_main_dis").innerHTML = "";
+                document.getElementById("oft_mtbf").innerHTML = "";
+                document.getElementById("rft_mtbf").innerHTML = this.round(this.ReconstructedFaultTree[0].mtbf, 5);
+                document.getElementById("oft_mtbr").innerHTML = "";
+                document.getElementById("rft_mtbr").innerHTML = this.round(this.ReconstructedFaultTree[0].mtbr, 5);
+                oper_avail_string = this.round(this.ReconstructedFaultTree[0].oper_avail * 100, 5).toString();
+                document.getElementById("oper_avail").innerHTML = oper_avail_string.concat(' %');
+            } else {
+                basicEventID = parseInt(this.getBasicEventNumber(event), 10);
+                console.log(basicEventID)
+                document.getElementById("oft_rel_dis").innerHTML = this.formatDistribution(this.OriginalFaultTree[basicEventID].reliability);
+                document.getElementById("rft_rel_dis").innerHTML = this.formatDistribution(this.ReconstructedFaultTree[basicEventID].reliability);
+                document.getElementById("oft_main_dis").innerHTML = this.formatDistribution(this.OriginalFaultTree[basicEventID].maintainability);
+                document.getElementById("rft_main_dis").innerHTML = this.formatDistribution(this.ReconstructedFaultTree[basicEventID].maintainability);
+                document.getElementById("oft_mtbf").innerHTML = this.round(this.OriginalFaultTree[basicEventID].mtbf, 5);
+                document.getElementById("rft_mtbf").innerHTML = this.round(this.ReconstructedFaultTree[basicEventID].mtbf, 5);
+                document.getElementById("oft_mtbr").innerHTML = this.round(this.OriginalFaultTree[basicEventID].mtbr, 5);
+                document.getElementById("rft_mtbr").innerHTML = this.round(this.ReconstructedFaultTree[basicEventID].mtbr, 5);
+                oper_avail_string = this.round(this.ReconstructedFaultTree[basicEventID].oper_avail * 100, 5).toString();
+                document.getElementById("oper_avail").innerHTML = oper_avail_string.concat(' %');
+            }
+        },
     }
-}
+})
 
-init();
